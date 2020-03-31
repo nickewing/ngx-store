fb3d500a26453a6144b13e5fdf1fc234eedbfad2
Jan Moxter <jan.moxter@moxter.net>
2020-03-31 13:16:28 -0500

    First working test

---
+++ projects/ngx-store/src/utility/webstorage.utility.spec.ts
@@ -0,0 +1,15 @@
+import {
+  localStorageUtility,
+  sessionStorageUtility,
+  cookiesStorageUtility,
+  sharedStorageUtility
+} from ".";
+
+describe("WebStorageUtility", () => {
+  it("getStorageName() should give proper values", () => {
+    expect(localStorageUtility.getStorageName()).toBe("localStorage");
+    expect(sessionStorageUtility.getStorageName()).toBe("sessionStorage");
+    expect(cookiesStorageUtility.getStorageName()).toBe("cookiesStorage");
+    expect(sharedStorageUtility.getStorageName()).toBe("sharedStorage");
+  });
+});
