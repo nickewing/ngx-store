dd714a39de39410021d5d47ff0a159bd3c6e3bbd
Jan Moxter <jan.moxter@moxter.net>
2020-03-31 12:43:01 -0500

    Restructure to angular 9

--- projects/ngx-store/src/public-api.ts
+++ projects/ngx-store/src/public-api.ts
@@ -9,6 +9,6 @@
 /**
  * Entry point for all public APIs of the package.
  */
-export * from './src/ngx-store';
+export * from './ngx-store';
 
 // This file only reexports content of the `src` folder. Keep it that way.
