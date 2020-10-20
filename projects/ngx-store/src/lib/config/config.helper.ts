c3eba13ce38e06538477ce19a745c1ff1c11222c
Jan Moxter <jan.moxter@moxter.net>
2020-10-19 20:59:21 -0500

    Check for localstorage availability

--- projects/ngx-store/src/lib/config/config.helper.ts
+++ projects/ngx-store/src/lib/config/config.helper.ts
@@ -1,15 +1,22 @@
 export const CONFIG_PREFIX = "NGX-STORE_";
 
 export class ConfigHelper {
+  public static isLocalStorageSupported() {
+    return (
+      typeof window["localStorage"] != "undefined" &&
+      window["localStorage"] != null
+    );
+  }
+
   public static getPrefix(): any {
-    if (localStorage) {
+    if (ConfigHelper.isLocalStorageSupported() ) {
       localStorage.getItem("NGX-STORE_prefix");
     }
     return CONFIG_PREFIX;
   }
 
   public static setPrefix(prefix: string): any {
-    if (localStorage) {
+    if (ConfigHelper.isLocalStorageSupported() ) {
       localStorage.setItem("NGX-STORE_prefix", prefix);
     }
     return CONFIG_PREFIX;
