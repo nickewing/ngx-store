c87d55024ead09ea8dd5aebc6f82b3b92871a27a
Jan Moxter <jan.moxter@moxter.net>
2020-10-19 21:45:44 -0500

    Check for localstorage

--- projects/ngx-store/src/lib/utility/utilities.ts
+++ projects/ngx-store/src/lib/utility/utilities.ts
@@ -1,14 +1,26 @@
-import { WebStorageUtility } from './webstorage.utility';
-import { Config } from '../config/config';
-import { cookiesStorage } from './storage/cookies-storage';
-import { SharedStorageUtility } from './shared-storage.utility';
-import { sharedStorage } from './storage/shared-storage';
+import { WebStorageUtility } from "./webstorage.utility";
+import { Config } from "../config/config";
+import { cookiesStorage } from "./storage/cookies-storage";
+import { SharedStorageUtility } from "./shared-storage.utility";
+import { sharedStorage } from "./storage/shared-storage";
 
-export const localStorageUtility: WebStorageUtility =
-    new WebStorageUtility(localStorage, Config.prefix, Config.previousPrefix);
-export const sessionStorageUtility: WebStorageUtility =
-    new WebStorageUtility(sessionStorage, Config.prefix, Config.previousPrefix);
-export const cookiesStorageUtility: WebStorageUtility =
-    new WebStorageUtility(cookiesStorage, Config.prefix, Config.previousPrefix);
-export const sharedStorageUtility: SharedStorageUtility =
-    new SharedStorageUtility(sharedStorage, Config.prefix, Config.prefix);
+export const localStorageUtility: WebStorageUtility = new WebStorageUtility(
+  typeof localStorage !== "undefined" ? localStorage : null,
+  Config.prefix,
+  Config.previousPrefix
+);
+export const sessionStorageUtility: WebStorageUtility = new WebStorageUtility(
+  sessionStorage,
+  Config.prefix,
+  Config.previousPrefix
+);
+export const cookiesStorageUtility: WebStorageUtility = new WebStorageUtility(
+  cookiesStorage,
+  Config.prefix,
+  Config.previousPrefix
+);
+export const sharedStorageUtility: SharedStorageUtility = new SharedStorageUtility(
+  sharedStorage,
+  Config.prefix,
+  Config.prefix
+);
